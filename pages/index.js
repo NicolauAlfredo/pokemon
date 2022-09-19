b9dfaf5ae7ed57f8cb41bdb52a3ea9c2dff280f8
Nicolau Alfredo <nicolaualfredoalfredo@gmail.com>
2022-09-19 20:14:02 +0100

    Adicionando a API das imagens ao projecto

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import styles from '../styles/Home.module.css'
 
+import Image from 'next/image'
+import Card from './components/Card'
+
+
+
 export async function getStaticProps() {
 
-  const maxPokemons = 251
+  const maxPokemons = 256
   const api = 'https://pokeapi.co/api/v2/pokemon/'
 
   const res = await fetch(`${api}?limit=${maxPokemons}`)
@@ -22,13 +27,21 @@ export async function getStaticProps() {
 
 export default function Home({ pokemons }) {
   return (
-    <div>
-      <h1>PokeNext</h1>
-      <ul>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>Poke<span>Next</span></h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokeNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{ pokemon.name }</li>
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   )
 }
